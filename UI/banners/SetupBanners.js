d42e2fb9751705e4d45e4345440c8b3167607fe9
assassin78476 <lucas.vrousseleau@gmail.com>
2025-09-29 14:29:57 +0200

    Update

---
+++ UI/banners/SetupBanners.js
@@ -0,0 +1,8 @@
+const setupBanners = {
+    helpBanner : 'https://media.discordapp.net/attachments/1397077990579372126/1416442022457249873/ChatGPT_Image_7_sept._2025_17_20_16.png?ex=68c6dbee&is=68c58a6e&hm=de1ad7a0707763d0a978ffdbdaeacbad793dda82409e2901dae6525856afab9d&=&format=webp&quality=lossless&width=795&height=795',
+    verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
+    ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
+    autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
+};
+
+module.exports = setupBanners;
---
+++ lavalink.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "Elysia-V4",
+      password: "youshallnotpass",
+      host: "lavalink.jirayu.net",
+      port:  13592,
+      secure: false
+    }
+};
